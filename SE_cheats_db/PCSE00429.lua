462ea6163f13d78edda66cc8ca4289bd4be444af
Rinnegatamante <rinnegatamante@gmail.com>
2016-09-22 21:13:26 +0200

    Added new cheats in SE database.
    Added new cheats in SE database.

---
+++ SE_cheats_db/PCSE00429.lua
@@ -0,0 +1,15 @@
+-- Tales of Hearts R (USA)
+-- Credits: Red7s
+
+cur_chts = {
+	{["name"]="Max Grade", ["offset"]=0x6160, ["file"]="TOH_DATA0000.bin", ["value"]=0x1869F, ["size"]=4},	
+	{["name"]="x99999 Gald", ["offset"]=0x615C, ["file"]="TOH_DATA0000.bin", ["value"]=0x1869F, ["size"]=4},
+	{["name"]="x99999 Kor/Shing Total", ["offset"]=0x32D0, ["file"]="TOH_DATA0000.bin", ["value"]=0x1869F, ["size"]=4},
+	{["name"]="x99999 Kor/Shing", ["offset"]=0x32AC, ["file"]="TOH_DATA0000.bin", ["value"]=0x1869F, ["size"]=4},
+	{["name"]="x99999 Beryl", ["offset"]=0x4B8C, ["file"]="TOH_DATA0000.bin", ["value"]=0x1869F, ["size"]=4},
+	{["name"]="x99999 Hisui", ["offset"]=0x3F1C, ["file"]="TOH_DATA0000.bin", ["value"]=0x1869F, ["size"]=4},
+	{["name"]="x99999 Kohaku", ["offset"]=0x38E4, ["file"]="TOH_DATA0000.bin", ["value"]=0x1869F, ["size"]=4},
+	{["name"]="x99999 Ines", ["offset"]=0x4554, ["file"]="TOH_DATA0000.bin", ["value"]=0x1869F, ["size"]=4},
+	{["name"]="x99999 Gall", ["offset"]=0x5E34, ["file"]="TOH_DATA0000.bin", ["value"]=0x1869F, ["size"]=4},
+	{["name"]="x99999 Kunzite", ["offset"]=0x51C4, ["file"]="TOH_DATA0000.bin", ["value"]=0x1869F, ["size"]=4}
+}
