6e78d835792c24f11041ccc536e0f7eed1427cfa
Rinnegatamante <rinnegatamante@gmail.com>
2016-09-25 10:23:45 +0200

    Added new cheats to SE database.
    Added new cheats to SE database.

---
+++ SE_cheats_db/PCSB00048.lua
@@ -0,0 +1,7 @@
+-- Ridge Racer (EUR)
+-- Credits: Slade
+
+cur_chts = {
+	{["name"]="500,000 Total Credits Earned", ["offset"]=0x8B, ["file"]="system0000.bin", ["value"]=0x7A120, ["size"]=4},
+	{["name"]="500,000 Credits", ["offset"]=0x8F, ["file"]="system0000.bin", ["value"]=0x7A120, ["size"]=4}
+}
---
+++ SE_cheats_db/PCSB00182.lua
@@ -0,0 +1,6 @@
+-- MUD: FIM Motorcross World Championship (EUR)
+-- Credits: Slade
+
+cur_chts = {
+	{["name"]="999,999 Credits", ["offset"]=0x2982, ["file"]="CAREER-0.DAT", ["value"]=0x0F423F, ["size"]=4}
+}
