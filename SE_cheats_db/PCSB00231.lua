9bd38087d49986f8a0c44c5e6eaf8d92aa1cf974
Rinnegatamante <rinnegatamante@gmail.com>
2016-09-28 21:48:10 +0200

    Added new cheats to SE database.
    Added new cheats to SE database.

---
+++ SE_cheats_db/PCSB00231.lua
@@ -0,0 +1,12 @@
+-- Alien Breed (EUR)
+-- Credits: Slade
+
+needs_crc32 = false
+
+cur_chts = {
+	{["name"]="999,999 Credits", ["offset"]=0x3C10, ["file"]="my_data.bin", ["value"]=0xF423F, ["size"]=4},
+	{["name"]="99 Lives", ["offset"]=0x3C2C, ["file"]="my_data.bin", ["value"]=0x63, ["size"]=1},
+	{["name"]="99 Health", ["offset"]=0x3C30, ["file"]="my_data.bin", ["value"]=0x63, ["size"]=1},
+	{["name"]="99 Keys", ["offset"]=0x3C34, ["file"]="my_data.bin", ["value"]=0x63, ["size"]=1},
+	{["name"]="Full Ammo", ["offset"]=0x3C38, ["file"]="my_data.bin", ["value"]=0x3DE0, ["size"]=4}
+}
