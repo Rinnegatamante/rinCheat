32b1e17ea2a6cec9806da27342efe7a7e37574e9
Rinnegatamante <rinnegatamante@gmail.com>
2016-10-01 11:55:48 +0200

    Added new cheats to SE database.
    Added new cheats to SE database.

---
+++ SE_cheats_db/PCSB00640.lua
@@ -0,0 +1,16 @@
+-- Akiba's Trip Undressed & Undead (EUR)
+-- Credits: Slade, Rasa39
+
+needs_crc32 = false
+
+cur_chts = {
+	{["name"]="999,999 XP", ["offset"]=0x60C, ["file"]="USR-DATA0000.BIN", ["value"]=0xF423F, ["size"]=4},
+	{["name"]="999,990 Money", ["offset"]=0xB51C, ["file"]="USR-DATA0000.BIN", ["value"]=0xF4236, ["size"]=4},
+	{["name"]="999 Upper Body Points", ["offset"]=0x124, ["file"]="USR-DATA0000.BIN", ["value"]=0x3E7, ["size"]=4},
+	{["name"]="999 Equipment Points", ["offset"]=0x5A4, ["file"]="USR-DATA0000.BIN", ["value"]=0x3E7, ["size"]=4},
+	{["name"]="999 Lower Body Points", ["offset"]=0x184, ["file"]="USR-DATA0000.BIN", ["value"]=0x3E7, ["size"]=4},
+	{["name"]="300 Base Attack", ["offset"]=0x608, ["file"]="USR-DATA0000.BIN", ["value"]=0x12C, ["size"]=4},
+	{["name"]="300 Base Defence", ["offset"]=0x604, ["file"]="USR-DATA0000.BIN", ["value"]=0x12C, ["size"]=4},
+	{["name"]="1000 Upper Durability", ["offset"]=0x988, ["file"]="USR-DATA0000.BIN", ["value"]=0x3E8, ["size"]=4},
+	{["name"]="1000 Lower Durability", ["offset"]=0x9E8, ["file"]="USR-DATA0000.BIN", ["value"]=0x3E8, ["size"]=4}
+}
---
+++ SE_cheats_db/PCSF00006.lua
@@ -0,0 +1,9 @@
+-- Everybody's Golf (EUR)
+-- Credits: Slade
+
+needs_crc32 = false
+
+cur_chts = {
+	{["name"]="50,000,000 Current Points", ["offset"]=0x58EF8, ["file"]="data2_EU.bin", ["value"]=0x2FAF080, ["size"]=4},
+	{["name"]="50,000,000 Total Points", ["offset"]=0x58EFC, ["file"]="data2_EU.bin", ["value"]=0x2FAF080, ["size"]=4}
+}
