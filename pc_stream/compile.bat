b7692858557cb9757f81540a722b127382b1c85a
Rinnegatamante <rinnegatamante@gmail.com>
2016-10-03 15:58:33 +0200

    Added icon to PC streamer application.
    Added icon to PC streamer application.

--- pc_stream/compile.bat
+++ pc_stream/compile.bat
@@ -1,1 +1,2 @@
-gcc SDL_win32_main.c main.c -o "rinCheat PSVITA streamer" -lopengl32 -lSDL -lSDL_image -DNO_STDIO_REDIRECT -lws2_32
+windres profile.rc -O coff -o icon.res
+gcc SDL_win32_main.c main.c icon.res -o "rinCheat PSVITA streamer" -lopengl32 -lSDL -lSDL_image -DNO_STDIO_REDIRECT -lws2_32
