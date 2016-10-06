e35a64e520551d13e778a2a27c662a742d517f6b
Slade1972 <Slade1972@users.noreply.github.com>
2016-10-06 21:21:43 +1100

    Update PCSE00291.lua
    Removed extraneous comma

--- SE_cheats_db/PCSE00291.lua
+++ SE_cheats_db/PCSE00291.lua
@@ -5,5 +5,5 @@ needs_crc32 = false
 
 cur_chts = {
 	{["name"]="99 Coffee", ["offset"]=0x520, ["file"]="data.bin", ["value"]=0x63, ["size"]=1},
-	{["name"]="99,999,999 Coins", ["offset"]=0x524, ["file"]="data.bin", ["value"]=0x5F5E0FF, ["size"]=4},
+	{["name"]="99,999,999 Coins", ["offset"]=0x524, ["file"]="data.bin", ["value"]=0x5F5E0FF, ["size"]=4}
 }
