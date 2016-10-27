87625ad4c22b939ef03396a9634f3b95c6e9996b
Rinnegatamante <rinnegatamante@gmail.com>
2016-10-27 10:25:26 +0200

    Added new cheats to the database.
    Added new cheats to the database.

---
+++ SE_cheats_db/PCSB00041.lua
@@ -0,0 +1,9 @@
+-- Dungeon Hunter: Alliance (EUR)
+-- Credits: Slade
+
+needs_crc32 = false
+
+cur_chts = {
+	{["name"]="9,999,999 Gold", ["offset"]=0x6B7, ["file"]="savefiles/active/dhangp-saves-1.savegame", ["value"]=0x98967F, ["size"]=4},
+	{["name"]="999 Crystals", ["offset"]=0x6BB, ["file"]="savefiles/active/dhangp-saves-1.savegame", ["value"]=0x3E7, ["size"]=2}
+}
---
+++ SE_cheats_db/PCSE00881.lua
@@ -0,0 +1,9 @@
+-- Gal*Gun Double Peace (USA)
+-- Credits: Slade
+
+needs_crc32 = false
+
+cur_chts = {
+	{["name"]="9,999,999 MP", ["offset"]=0x20, ["file"]="gg2shootingSavedata0001.bin", ["value"]=0x98967F, ["size"]=4},
+	{["name"]="99,999 Angel Feathers", ["offset"]=0x30, ["file"]="gg2shootingSavedata0001.bin", ["value"]=0x1869F, ["size"]=4}
+}
