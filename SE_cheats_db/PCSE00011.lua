36d4dd5210c286f6649f8671723c8237d8ddeb70
Rinnegatamante <rinnegatamante@gmail.com>
2016-12-03 12:16:15 +0100

    Added new cheats to the database.
    Added new cheats to the database.

---
+++ SE_cheats_db/PCSE00011.lua
@@ -0,0 +1,16 @@
+-- Silent Hill: Book of Memories (USA)
+-- Credits: Slade
+
+needs_crc32 = false
+
+warning = "Keys reset to zero every level."
+
+cur_chts = {
+	{["name"]="99 Health Packs", ["offsets"]={0x611B}, ["file"]="data0000.bin", ["value"]=0x63, ["size"]=1},
+	{["name"]="99 Ammo Packs", ["offsets"]={0x6113}, ["file"]="data0000.bin", ["value"]=0x63, ["size"]=1},
+	{["name"]="99 Wrenches", ["offsets"]={0x6123}, ["file"]="data0000.bin", ["value"]=0x63, ["size"]=1},
+	{["name"]="999 Upgrade Points", ["offsets"]={0x632F}, ["file"]="data0000.bin", ["value"]=0x3E7, ["size"]=2},
+	{["name"]="Level 99", ["offsets"]={0x6337}, ["file"]="data0000.bin", ["value"]=0x63, ["size"]=1},
+	{["name"]="9,999,999 MR", ["offsets"]={0x633B}, ["file"]="data0000.bin", ["value"]=0x98967F, ["size"]=4},
+	{["name"]="99 Keys", ["offsets"]={0x6333}, ["file"]="data0000.bin", ["value"]=0x63, ["size"]=1}
+}
