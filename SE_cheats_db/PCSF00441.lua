6e51b9c4c68aabb7cbec3d1f193a4c10d9b41dc8
Rinnegatamante <rinnegatamante@gmail.com>
2016-12-05 10:32:56 +0100

    Added new cheats to the database.
    Added new cheats to the database.

---
+++ SE_cheats_db/PCSF00441.lua
@@ -0,0 +1,10 @@
+-- Looney Tunes Galactic Sports (AUS)
+-- Credits: Slade
+
+needs_crc32 = false
+
+warning =  nil
+
+cur_chts = {
+   {["name"]="99,999 Coins", ["offsets"]={0xC}, ["file"]="data0001.bin", ["value"]=0x1869F, ["size"]=4}
+}
